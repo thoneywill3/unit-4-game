f2f05b8271d8216d84bf543ffbdbfdf50d9a2cc5
thoneywill3 <43575282+thoneywill3@users.noreply.github.com>
2018-10-27 19:08:30 -0500

    Add files via upload

---
+++ assets/game.js
@@ -0,0 +1,26 @@
+        var randomNumber = getRandomNumber(100);
+
+        function getRandomNumber(upper){
+            var num = Math.floor(Math.random()*upper) + 1;
+            return num;
+        }
+
+        function crystal1()
+        {
+            //add logic to add random points from crystal1 button to player's total score
+        }
+
+        function crystal2()
+        {
+            //add logic to add random points from crystal2 button to player's total score
+        }
+
+        function crystal3()
+        {
+            //add logic to add random points from crystal3 button to player's total score
+        }
+
+        function crystal4()
+        {
+            //add logic to add random points from crystal4 button to player's total score
+        }
